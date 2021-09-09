281e86faa9514da1816fa16e5e2cc0cc52f61777
Nikita <fanban3377@gmail.com>
2021-09-09 13:44:27 +0500

    Initial commit

---
+++ app/src/main/java/com/susu/susuapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.susu.susuapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
