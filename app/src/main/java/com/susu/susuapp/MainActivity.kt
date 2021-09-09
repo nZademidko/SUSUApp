a9ff2f7341eaaf7e5d2237ba41172a3e33d9e8a3
Nikita <fanban3377@gmail.com>
2021-09-09 15:45:46 +0500

    NavComponent commit

--- app/src/main/java/com/susu/susuapp/MainActivity.kt
+++ app/src/main/java/com/susu/susuapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.susu.susuapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView).setupWithNavController(
+            navController = navController
+        )
     }
+
+    override fun onSupportNavigateUp() = navController.navigateUp() || super.onSupportNavigateUp()
+
 }
